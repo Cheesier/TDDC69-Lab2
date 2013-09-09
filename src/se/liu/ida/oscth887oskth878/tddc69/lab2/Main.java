a00634fb3dd4bd22fa625be60322adef0c8b3bf1
Oscar Thunberg <ostenip@gmail.com>
2013-09-09 16:33:27 +0200

    Added boilerplate Classes

---
+++ src/se/liu/ida/oscth887oskth878/tddc69/lab2/Main.java
@@ -0,0 +1,14 @@
+package se.liu.ida.oscth887oskth878.tddc69.lab2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ostenip
+ * Date: 9/9/13
+ * Time: 4:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
