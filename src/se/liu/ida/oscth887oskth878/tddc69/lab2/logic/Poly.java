f31cf7766552cd2fca8fcc8b17b9b57bab6b6150
Oscar Thunberg <ostenip@gmail.com>
2013-09-12 15:07:48 +0200

    Parse the Poly where it should be parsed

--- src/se/liu/ida/oscth887oskth878/tddc69/lab2/logic/Poly.java
+++ src/se/liu/ida/oscth887oskth878/tddc69/lab2/logic/Poly.java
@@ -1,5 +1,7 @@
 package se.liu.ida.oscth887oskth878.tddc69.lab2.logic;
 
+import se.liu.ida.oscth887oskth878.tddc69.lab2.math.Vec2;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ostenip
@@ -11,8 +13,14 @@ public class Poly {
     private SquareType[][] squares;
     private SquareType.Shape shape;
 
-    public Poly(SquareType[][] squares) {
-        this.squares = squares;
+    public Poly(String shape, String pattern) {
+        this.shape = SquareType.Shape.valueOf(shape);
+
+        parsePoly(pattern);
+    }
+
+    public SquareType.Shape getShape() {
+        return shape;
     }
 
     public void rotate(boolean clockwise) {
@@ -23,4 +31,52 @@ public class Poly {
             // rotate it ccw
         }
     }
+
+    private void parsePoly(String pattern) {
+        Vec2 dim = getDimensions(pattern);
+        squares = new SquareType[dim.x][dim.y];
+
+        int x = 0, y = 0;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            switch (pattern.charAt(i)) {
+                case '#':
+                    squares[x][y] = new SquareType(this.shape);
+                    x++;
+                    break;
+                case ' ':
+                    squares[x][y] = null;
+                    x++;
+                    break;
+                case '\n':
+                    x = 0;
+                    y++;
+                    break;
+                default:
+                    System.err.println("Found invalid sign '" + pattern.charAt(i) + "' while parsing Poly.");
+                    break;
+            }
+        }
+
+    }
+
+    private Vec2 getDimensions(String pattern) {
+        Vec2 dim = new Vec2();
+        boolean foundWidth = false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (pattern.charAt(i) == '\n') {
+                if (!foundWidth) {
+                    dim.x = i;
+                    foundWidth = true;
+                }
+                dim.y++;
+            }
+        }
+
+        dim.y++; // we don't use a newline char at the last row, compensate for that
+        return dim;
+    }
+
+
 }
--- src/se/liu/ida/oscth887oskth878/tddc69/lab2/logic/TetrominoMaker.java
+++ src/se/liu/ida/oscth887oskth878/tddc69/lab2/logic/TetrominoMaker.java
@@ -1,8 +1,5 @@
 package se.liu.ida.oscth887oskth878.tddc69.lab2.logic;
 
-import se.liu.ida.oscth887oskth878.tddc69.lab2.math.Vec2;
-
-import java.util.Iterator;
 import java.util.Random;
 
 /**
@@ -13,11 +10,16 @@ import java.util.Random;
  * To change this template use File | Settings | File Templates.
  */
 public class TetrominoMaker {
-    private int numberOfTypes = TetrominoBlueprints.blueprints.length;
-    private Poly[] polies = new Poly[TetrominoBlueprints.blueprints.length];
+    private Poly[] polies;
+
+    public TetrominoMaker(String[][] blueprints) {
+        polies = new Poly[blueprints.length];
+
+        loadBlueprints(blueprints);
+    }
 
     public int getNumberOfTypes() {
-        return numberOfTypes;
+        return this.polies.length;
     }
 
     public Poly getPoly(int n) {
@@ -26,45 +28,12 @@ public class TetrominoMaker {
 
     public Poly getRandomPoly() {
         Random rand = new Random();
-        return polies[rand.nextInt(numberOfTypes)];
+        return polies[rand.nextInt(polies.length)];
     }
 
-    public void loadBlueprints() {
-        for (int i = 0; i < numberOfTypes; i++) {
-            parsePoly(TetrominoBlueprints.blueprints[i][0], TetrominoBlueprints.blueprints[i][1]);
+    private void loadBlueprints(String[][] blueprints) {
+        for (int i = 0; i < blueprints.length; i++) {
+            polies[i] = new Poly(blueprints[i][0], blueprints[i][1]);
         }
-    }
-
-    private Poly parsePoly(String name, String pattern) {
-        Poly poly = new Poly(patternToSquare(name, pattern));
-        return null;
-    }
-
-    private SquareType[][] patternToSquare(String name, String pattern) {
-        Vec2 dim = getDimensions(pattern);
-
-        SquareType[][] shape = new SquareType[dim.x][dim.y];
-
-        return null;
-    }
-
-    private Vec2 getDimensions(String pattern) {
-        Vec2 dim = new Vec2();
-        boolean foundWidth = false;
-
-        for (int i = 0; i < pattern.length(); i++) {
-            if (pattern.charAt(i) == '\n') {
-                if (!foundWidth) {
-                    dim.x = i;
-                    foundWidth = true;
-                }
-                dim.y++;
-            }
-        }
-
-        dim.y++; // we don't use a newline char at the last row, compensate for that
-
-        System.out.println(dim);
-        return dim;
     }
 }
